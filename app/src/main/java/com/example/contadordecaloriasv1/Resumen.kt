54803f7b4caf6d3efaf87dc4e3204dc204a1453a
Gonzalo <trontzal@gmail.com>
2024-09-06 22:16:50 +0200

    arreglado

--- app/src/main/java/com/example/contadordecaloriasv1/Resumen.kt
+++ app/src/main/java/com/example/contadordecaloriasv1/Resumen.kt
@@ -1,6 +1,7 @@
 package com.example.contadordecaloriasv1
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,13 @@ class Resumen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resumen)
+
+        val caloriasTextView: TextView = findViewById(R.id.idCaloriasObjetivo)
+
+
+        val caloriasObjetivo = 2400
+
+        caloriasTextView.text = "$caloriasObjetivo kcal"
 
     }
 }
